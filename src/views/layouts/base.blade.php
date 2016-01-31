126eb94d7ac3c98aa84f84b4d05e088ad003acc7
Ben Hirsch <benhirsch@gmail.com>
2016-01-31 21:02:20 +0000

    universal csrf token added for all ajax requests

--- src/views/layouts/base.blade.php
+++ src/views/layouts/base.blade.php
@@ -5,6 +5,7 @@
     <title>{{ $app_data['name'].' | '.$page_title }}</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 
     <!-- Bootstrap -->
     @section('head')
@@ -54,6 +55,8 @@
       var curr_subpanel  = '{{ isset($curr_subpanel) ? $curr_subpanel : '' }}';
       var curr_method = '{{ $curr_method }}';
       var curr_id = '{{ $curr_id }}';
+
+      $(document).ready(function() { $.ajaxSetup({ headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') }}); });
     </script>
 
   @show
